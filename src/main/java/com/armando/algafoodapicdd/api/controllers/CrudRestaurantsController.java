c5ffa792b25a9a31b71830fa31496ec27565c9cf
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2021-04-07 16:41:55 -0300

    CDD count in CrudRestaurantsController

--- src/main/java/com/armando/algafoodapicdd/api/controllers/CrudRestaurantsController.java
+++ src/main/java/com/armando/algafoodapicdd/api/controllers/CrudRestaurantsController.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// Carga intrínsica = 5; Limite = 7
 @RestController
 @RequestMapping("/restaurants")
 public class CrudRestaurantsController {
@@ -25,6 +26,7 @@ public class CrudRestaurantsController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Transactional
+    // Carga: +1 (RestaurantResponse) +1 (RestaurantRequest)
     public RestaurantResponse insert(@RequestBody @Valid RestaurantRequest restaurantRequest) {
         Restaurant restaurant = restaurantRequest.toModel(manager);
         manager.persist(restaurant);
@@ -33,10 +35,12 @@ public class CrudRestaurantsController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    // Carga: +1 (RestaurantSummaryResponse)
     public List<RestaurantSummaryResponse> list() {
         return manager
                 .createQuery("SELECT restaurant from Restaurant restaurant", Restaurant.class)
                 .getResultList()
+                // Carga: +1 (função como argumento no map)
                 .stream().map(restaurant -> new RestaurantSummaryResponse(restaurant))
                 .collect(Collectors.toList());
     }
@@ -45,6 +49,7 @@ public class CrudRestaurantsController {
     @ResponseStatus(HttpStatus.OK)
     public RestaurantResponse findById(@PathVariable Long restaurantId) {
         Restaurant restaurant = manager.find(Restaurant.class, restaurantId);
+        // Carga: +1 (branch if)
         if (restaurant == null) throw new EntityNotFoundException("Restaurante não encontrado");
         return new RestaurantResponse(restaurant);
     }
