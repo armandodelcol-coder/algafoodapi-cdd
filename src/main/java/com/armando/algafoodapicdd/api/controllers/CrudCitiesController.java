32aefc5e1cf50843ba30eb8418c48cb0ed8a177d
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2021-04-07 22:07:28 -0300

    Complete essential CRUD of Cities

--- src/main/java/com/armando/algafoodapicdd/api/controllers/CrudCitiesController.java
+++ src/main/java/com/armando/algafoodapicdd/api/controllers/CrudCitiesController.java
@@ -1,22 +1,26 @@
 package com.armando.algafoodapicdd.api.controllers;
 
+import com.armando.algafoodapicdd.api.exceptionhandler.CustomErrorResponseBody;
 import com.armando.algafoodapicdd.api.model.request.CityRequest;
 import com.armando.algafoodapicdd.api.model.response.CityResponse;
 import com.armando.algafoodapicdd.api.validator.CityAlreadyExistsInStateValidator;
 import com.armando.algafoodapicdd.domain.model.City;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// Carga intrínsica = 5; Limite = 7
+// Carga intrínsica = 8; Limite = 7
 @RestController
 @RequestMapping("/cities")
 public class CrudCitiesController {
@@ -47,10 +51,59 @@ public class CrudCitiesController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<CityResponse> list() {
-        return manager.createQuery("SELECT city from City city", City.class).getResultList()
+        return manager.createQuery("SELECT city from City city join fetch city.state", City.class).getResultList()
                 // Carga: +1 (função como argumento no map)
                 .stream().map(city -> new CityResponse(city))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{cityId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CityResponse findById(@PathVariable Long cityId) {
+        City city = manager.find(City.class, cityId);
+        checkCityExistence(city);
+        return new CityResponse(city);
+    }
+
+    @PutMapping("/{cityId}")
+    @ResponseStatus(HttpStatus.OK)
+    @Transactional
+    public CityResponse update(
+            @PathVariable Long cityId,
+            @RequestBody @Valid CityRequest cityRequest
+    ) {
+        City city = manager.find(City.class, cityId);
+        checkCityExistence(city);
+        city.setPropertiesToUpdate(cityRequest, manager);
+        manager.persist(city);
+        return new CityResponse(city);
+    }
+
+    @DeleteMapping("/{cityId}")
+    @Transactional
+    public ResponseEntity<?> delete(@PathVariable Long cityId) {
+        City city = manager.find(City.class, cityId);
+        checkCityExistence(city);
+        // Carga: +1 (branch if)
+        if (city.hasAnyRestaurantWithThisCityRegistered(manager)) {
+            return ResponseEntity.badRequest().body(
+                    // Carga: +1 (CustomErrorResponseBody)
+                    new CustomErrorResponseBody(
+                            HttpStatus.BAD_REQUEST.value(),
+                            HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                            "Existem endereços cadastrados com essa cidade.",
+                            OffsetDateTime.now()
+                    )
+            );
+        }
+
+        manager.remove(city);
+        return ResponseEntity.noContent().build();
+    }
+
+    private void checkCityExistence(City city) {
+        // Carga: +1 (branch if)
+        if (city == null) throw new EntityNotFoundException("Cidade não encontrada.");
     }
 
 }
--- src/main/java/com/armando/algafoodapicdd/domain/model/City.java
+++ src/main/java/com/armando/algafoodapicdd/domain/model/City.java
@@ -1,9 +1,12 @@
 package com.armando.algafoodapicdd.domain.model;
 
+import com.armando.algafoodapicdd.api.model.request.CityRequest;
+import org.springframework.util.Assert;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
-// Carga intrínsica = 1; Limite = 9
+// Carga intrínsica = 3; Limite = 9
 @Entity
 @Table(name = "tb_city")
 public class City {
@@ -39,6 +42,23 @@ public class City {
 
     public State getState() {
         return state;
+    }
+
+    // Carga: +1 (CityRequest)
+    public void setPropertiesToUpdate(CityRequest cityRequest, EntityManager manager) {
+        State state = manager.find(State.class, cityRequest.getStateId());
+        Assert.state(state != null, "Não é permitido salvar uma Cidade com um Estado inexistente");
+
+        this.name = cityRequest.getName();
+        this.state = state;
+    }
+
+    public boolean hasAnyRestaurantWithThisCityRegistered(EntityManager manager) {
+        // Carga: +1 (Restaurant)
+        return !manager.createQuery("SELECT restaurant FROM Restaurant restaurant WHERE restaurant.address.city.id = :cityId", Restaurant.class)
+                .setParameter("cityId", this.id)
+                .getResultList()
+                .isEmpty();
     }
 
 }
